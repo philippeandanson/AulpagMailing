3ffc79cf7b47148f2963467ba093d5f30a11cb1a
Expert BI <philippe.andanson@gmail.com>
2022-06-21 21:17:06 +0200

    Ajout documents envoyer

---
+++ AulpagMailing/Views/DocumentsEnvoyes.xaml.cs
@@ -0,0 +1,19 @@
+﻿using AulpagMailing.ViewModels;
+using System.Windows;
+
+namespace AulpagMailing.Views
+{
+   
+    public partial class DocumentsEnvoyes : Window
+    {
+        public DocumentsEnvoyes() { }
+
+        public DocumentsEnvoyes(MailingsViewModel viewModel)
+        {
+            InitializeComponent();
+            this.DataContext = viewModel;
+        }
+    }
+
+    
+}
